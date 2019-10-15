b0a696ec69ef677decbff653f1ea0e413b3a7678
fr.Innocent <brother.symeon@gmail.com>
2019-10-15 19:41:56 -0400

    added unti testing in the pages

---
+++ public/qa/tests-global.js
@@ -0,0 +1,8 @@
+
+
+suite('Global Tests', function(){
+  test('page has valid title', function(){
+    console.log(`document title= ${document.title}`)
+    assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO', 'should be valid');
+  })
+})
