f5cc87030bd16d5ebd0e1410201964a99aea341e
fr.Innocent <brother.symeon@gmail.com>
2019-10-15 06:31:39 -0400

    moved furtune cookie functionality to a module

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fortune = require('../lib/fortune');
+
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Meadowlark Travel' });
 });
 router.get('/about', function(req, res, next){
-  res.render('about');
+  var randomFortune = fortune.getFortune();
+  res.render('about', {fortune: randomFortune});
 })
 
 module.exports = router;
