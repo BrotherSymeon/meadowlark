e8a3a366cb9be4c90bcf396c583157f7e92c8994
fr.Innocent <brother.symeon@gmail.com>
2019-10-18 20:58:14 -0400

    added put and delete verbs to api

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,32 @@ var tours = [
 
 ]
 
+// get all the tours in json format
 router.get('/tours', function(req, res){
   res.json(tours);
+});
+
+// update a tour
+router.put('/api/tour/:id', function(req, res){
+  var p = tours.some(function(p){ return p.id === req.params.id; });
+  if(p){
+    if(req.query.name) p.name = req.query.name;
+    if(req.query.price) p.price = req.query.price;
+    res.json({success: true});
+  } else {
+    res.json({error: 'No such tour exists'});
+  }
+});
+
+//delete a tour
+router.delete('/api/tour/:id', function(req, res) {
+  var found = tours.findIndex(function(tour){ return tour.id === req.params.id; });
+  if(found >= 0){
+    tours.splice(found, 1);
+    res.json({ success: true });
+  } else {
+    res.json({ error: 'no such tour'});
+  }
 });
 
 module.exports = router;
