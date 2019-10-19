f4ab8d7d19434583062e24c55b065723ac8c3332
fr.Innocent <brother.symeon@gmail.com>
2019-10-18 22:59:40 -0400

    got the weather partial working

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
+var debug = require('debug')('meadowlark:server');
+
+var weather = require('./lib/weather');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +15,7 @@ var apiRouter = require('./routes/api');
 var app = express();
 
 // view engine setup
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -22,14 +27,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
   next();
 });
+
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,5 +79,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('500');
 });
+
+app.use(function(req, res, next){
+  
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals = getWeatherData();
+  next();
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,41 @@
 var express = require('express');
 var fortune = require('../lib/fortune');
+var debug = require('debug')('meadowlark:server');
+
 
 var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meadowlark Travel' });
+router.get('/', function (req, res, next) {
+  try {
+    debug("getting index", res.locals.partials.weather)
+    res.render('index', { title: 'Meadowlark Travel' });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+
 });
-router.get('/about', function(req, res, next){
+router.get('/about', function (req, res, next) {
   var randomFortune = fortune.getFortune();
   res.render('about', {
-    fortune: randomFortune, 
+    fortune: randomFortune,
     title: 'Meadowlark Travel',
     pageTestScript: '/qa/tests-about.js'
   });
 });
-router.get('/headers', function(req,res){
-  res.set('Content-Type','text/plain');
+router.get('/headers', function (req, res) {
+  res.set('Content-Type', 'text/plain');
   var s = '';
-  for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+  for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
   res.send(s);
 });
-router.get('/tours/hood-river', function(req, res){
+router.get('/tours/hood-river', function (req, res) {
   res.render('tours/hood-river', { title: 'Meadowlark Travel' });
 });
-router.get('/tours/request-group-rate', function(req, res){
-  res.render('tours/request-group-rate', {title: 'Meadowlark Travel'});
+router.get('/tours/request-group-rate', function (req, res) {
+  res.render('tours/request-group-rate', { title: 'Meadowlark Travel' });
 });
 
 module.exports = router;
